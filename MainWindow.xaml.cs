83715b8317e77b59e8cbe02eb5b97dfcceb0c7bf
danussik <121427601+danussik@users.noreply.github.com>
2024-05-11 11:51:20 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLesson1_bvv
+{
+    public partial class MainWindow : Window
+    {
+        private const string login = "admin";
+        private const string password = "123456";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (loginTextBox.Text.Equals(login) && passPasswordBox.Password.Equals(password))
+            {
+                MessageBox.Show("Authorization successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Authorization failed!", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
